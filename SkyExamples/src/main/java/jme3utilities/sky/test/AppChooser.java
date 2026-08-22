fe4c113e112831323ae1923d8ba0ae70cc0e27fc
stephengold <sgold@sonic.net>
2026-08-22 12:34:56 -0700

    AppChooser:  disable MSAA

--- SkyExamples/src/main/java/jme3utilities/sky/test/AppChooser.java
+++ SkyExamples/src/main/java/jme3utilities/sky/test/AppChooser.java
@@ -139,7 +139,7 @@ final class AppChooser extends GuiApplication {
         AppSettings settings = new AppSettings(loadDefaults);
         settings.setAudioRenderer(null);
         settings.setResizable(true);
-        settings.setSamples(4); // anti-aliasing
+        settings.setSamples(1); // anti-aliasing
         settings.setTitle(title); // Customize the window's title bar.
         application.setSettings(settings);
         /*
